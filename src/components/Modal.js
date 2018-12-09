0e49e873c993e950e8ba9fb1502b8df5ae26daa7
Harry Saito <hhsaito@Harrys-iMac.local>
2018-12-09 00:26:05 -0800

    Fix image.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Modal = ( props ) => {
-
     return (
         <div id="modal-backdrop" role="dialog" aria-labelledby="dialog_title" aria-hidden={props.showModal ? 'false': 'true'} className={props.showModal ? 'opened': null}>
             <div id="modal-box" className={props.showModal ? 'show-box': null}>
@@ -10,7 +9,7 @@ const Modal = ( props ) => {
                     <div className="biography">
                         <h2 id="dialog_title">{props.modalBio.name}</h2>
                         <p>{props.modalBio.title}</p>
-                        <img alt={props.modalBio.name} src={`/images/bios/${props.modalBio.name}.jpg`} />
+                        <img alt={props.modalBio.name} src={props.modalBio.name ? `/images/bios/${props.modalBio.name}.jpg` : null} />
                         <p className="popup__close"><a href="#close" onClick={props.clickClosed} aria-label="Close biography">×</a></p>
                     </div>
                 </div>
